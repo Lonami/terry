b267db03b9b2428a00a28b41bd19ec0ba1856c59
Lonami Exo <totufals@hotmail.com>
2020-05-26 16:01:49 +0200

    Create a test to make sure we can deserialize the net dump

--- terraria-protocol/src/lib.rs
+++ terraria-protocol/src/lib.rs
@@ -3,3 +3,21 @@ pub mod packets;
 pub mod structures;
 
 pub(crate) use structures::{Deserializable, Serializable, SliceCursor};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_remote_deserialization() {
+        let mut data = include_bytes!("../test-data/remote.bin").to_vec();
+
+        let mut index = 0;
+        while index < data.len() {
+            // TODO don't reimplement this here
+            let packet_len = u16::from_le_bytes([data[index], data[index + 1]]) as usize;
+            packets::Packet::from_slice(&mut data[index + 2..index + packet_len]);
+            index += packet_len;
+        }
+    }
+}
