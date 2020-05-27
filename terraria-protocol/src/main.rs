8ed6b7db557c5307a08e1ce3dc92b6404537a380
Lonami Exo <totufals@hotmail.com>
2020-05-27 12:51:27 +0200

    Simplify hex parsing

--- terraria-protocol/src/main.rs
+++ terraria-protocol/src/main.rs
@@ -1,19 +1,10 @@
 use std::io::{self, BufRead};
 use terraria_protocol::packets;
 
-fn hex_digit_to_n(digit: u8) -> u8 {
-    match digit {
-        n @ b'0'..=b'9' => n - b'0',
-        n @ b'a'..=b'f' => 10 + n - b'a',
-        n @ b'A'..=b'F' => 10 + n - b'A',
-        n => panic!(format!("'{}' is not a hex digit", n as char)),
-    }
-}
-
 fn parse_hex(hex: &str) -> Vec<u8> {
     let mut result = Vec::with_capacity(hex.len() / 2);
-    hex.as_bytes().chunks(2).for_each(|n| {
-        result.push(hex_digit_to_n(n[0]) << 4 | hex_digit_to_n(n[1]));
+    (0..hex.len()).step_by(2).for_each(|i| {
+        result.push(u8::from_str_radix(&hex[i..i + 2], 16).expect("bad hex"));
     });
     result
 }
