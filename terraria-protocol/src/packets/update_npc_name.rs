78f979de154cc8aec2dbbaa080b3fa2318688282
Lonami Exo <totufals@hotmail.com>
2021-10-02 19:46:01 +0200

    Change UpdateNpcName to read remaining info if available

--- terraria-protocol/src/packets/update_npc_name.rs
+++ terraria-protocol/src/packets/update_npc_name.rs
@@ -7,11 +7,8 @@ use crate::SliceCursor;
 #[derive(Debug)]
 pub struct UpdateNpcName {
     pub npc_id: i16,
-    // TODO we don't know if we're receiving or not!
-    // Only if client is receiving packet
-    //pub name: String,
-    // Only if client is receiving packet
-    //pub townnpcvariationindex: i32,
+    pub name: Option<String>,
+    pub town_npc_variation_idx: Option<i32>,
 }
 
 impl PacketBody for UpdateNpcName {
@@ -19,11 +16,24 @@ impl PacketBody for UpdateNpcName {
 
     fn write_body(&self, cursor: &mut SliceCursor) {
         cursor.write(&self.npc_id);
+        if let Some((name, vi)) = self.name.as_ref().zip(self.town_npc_variation_idx) {
+            cursor.write(name);
+            cursor.write(&vi);
+        }
     }
 
     fn from_body(cursor: &mut SliceCursor) -> Self {
+        let npc_id = cursor.read();
+        let (name, town_npc_variation_idx) = if cursor.eof() {
+            (None, None)
+        } else {
+            (Some(cursor.read()), Some(cursor.read()))
+        };
+
         Self {
-            npc_id: cursor.read(),
+            npc_id,
+            name,
+            town_npc_variation_idx,
         }
     }
 }
