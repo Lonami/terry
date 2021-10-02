2298e4d4ea5460e74b9062c16d2b0a7ce8c0072c
Lonami Exo <totufals@hotmail.com>
2021-10-02 19:46:49 +0200

    Show more info and don't panic on the remote.bin test
    This is actually a great way to update the code to a newer version.

--- terraria-protocol/src/lib.rs
+++ terraria-protocol/src/lib.rs
@@ -23,10 +23,13 @@ mod tests {
         while index < data.len() {
             // TODO don't reimplement this here
             let packet_len = u16::from_le_bytes([data[index], data[index + 1]]) as usize;
+            eprintln!(
+                "checking tag {}: {:02x?}",
+                data[index + 2],
+                &data[index + 3..index + packet_len]
+            );
             packets::Packet::from_slice(&mut data[index + 2..index + packet_len]);
             index += packet_len;
         }
-
-        todo!("make sure we found a certain number of present tiles")
     }
 }
